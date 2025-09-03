e088f47eabd90c9fc5c0ebfd295a3e402a75e332
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-09-03 21:09:02 +0330

    change role

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,11 +1,3 @@
-import type { ReactElement } from 'react';
-
-import styles from '@/components/Banner/Banner.module.css';
-
-export default function Banner(): ReactElement {
-  return (
-    <div>
-      <div className={styles.container}></div>
-    </div>
-  );
+export default function Banner() {
+  return <div>Banner</div>;
 }
