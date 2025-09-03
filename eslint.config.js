7d60bb1bbd8e783d7c4413b33f74a299039f9948
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-09-03 12:40:53 +0330

    message

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,10 +17,17 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+     prettierConfig
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins:{
+      prettier:prettierPlugin
+    },
+    rules:{
+      'prettier/prettier':'error'
+    }
   },
 ])
